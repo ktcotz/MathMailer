1d915905228746a5c89dcc33b0ff2be50b230e91
ktcotz <naskret.kamil@gmail.com>
2025-02-02 20:59:24 +0100

    feat: supabase-admin & reset link
    Work mostly around supabase connection via admin, generating
    reset password link, hbs templates.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MailModule } from './mail/mail.module';
+import { SupabaseModule } from './supabase/supabase.module';
 
 @Module({
-  imports: [MailModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [MailModule, ConfigModule.forRoot({ isGlobal: true }), SupabaseModule],
 })
 export class AppModule {}
--- src/env.d.ts
+++ src/env.d.ts
@@ -3,5 +3,7 @@ declare namespace NodeJS {
     SERVER_PORT: string;
     EMAIL_FROM_USER: string;
     EMAIL_FROM_PASS: string;
+    SUPABASE_URL: string;
+    SUPABASE_SUPER_KEY: string;
   }
 }
